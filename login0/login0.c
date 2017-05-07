3d9265d155b974ea6ba2366bf12a6abd57ee70ce
calebreseau <arthur.labalec@gmail.com>
2017-05-07 19:59:53 +0100

    new crackme

--- login0/login0.c
+++ login0/login0.c
@@ -5,17 +5,17 @@ int main(int argc, char *argv[])
 {
 	volatile int group;
 	char buffer[64];
-	group=0x75736572; //user
+	group=0; //user
 	if (argc==2)
 	{
 		strcpy(buffer,argv[1]);
-		if (group==0x61646d696e)
+		if (group==0x726f6f74)
 		{
-			printf("Hello, admin %s\n",argv[1]);
+			printf("Hello, root %s\n",argv[1]);
 		}
 		else
 		{
-			printf("Hello, user %s\n",argv[1]);
+			printf("Hello, user (%08x) %s\n",group,argv[1]);
 		}
 	}
 	else
@@ -23,3 +23,12 @@ int main(int argc, char *argv[])
 		printf("Usage : <username>\n");
 	}
 }
+
+
+
+
+
+
+
+
+//\x0a\x0d\x0a\x0d\x0a\x0d\x0a\x0d\x0a\x0d\x0a\x0d\x0a\x0d\x0a\x0d\x0a\x0d\x0a\x0d\x0a\x0d\x0a\x0daaaatoor
